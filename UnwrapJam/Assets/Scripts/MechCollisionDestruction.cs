227bc2565134fe6042d691dcf342970e8801e9c1
Jonathon <jon.mayes@outlook.com>
2024-02-14 15:59:38 +0100

    everything working

--- UnwrapJam/Assets/Scripts/MechCollisionDestruction.cs
+++ UnwrapJam/Assets/Scripts/MechCollisionDestruction.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class MechCollisionDestruction : MonoBehaviour
 {
+    Collider parryCollider; //used to see if the parry is being used aka the collider is active
+
+    private void Awake()
+    {
+        parryCollider = GetComponent<Collider>();
+    }
+
+   
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.TryGetComponent<IDestructible>(out IDestructible destructible))
+        if(other.gameObject.TryGetComponent<IDestructible>(out IDestructible destructible) && parryCollider.enabled)
         {
             destructible.Destruct();
         }
