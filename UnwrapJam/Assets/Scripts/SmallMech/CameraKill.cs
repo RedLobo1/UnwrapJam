0e137ace8e6fbaa17d4005274d20baca6c35d148
Kálmán Pajcsics <105979789+KalmanPA@users.noreply.github.com>
2024-02-15 04:03:34 +0100

    Camera kill setup

---
+++ UnwrapJam/Assets/Scripts/SmallMech/CameraKill.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraKill : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("BigMech"))
+        {
+            Debug.Log("GameOver");
+        }
+
+        if (other.gameObject.CompareTag("SmallBoy"))
+        {
+            Debug.Log("SmallBoyDead");
+        }
+    }
+}
