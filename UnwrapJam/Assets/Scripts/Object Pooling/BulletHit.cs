208158f25720d1d10981fabe9a6ff81ee2ab2b1d
Kálmán Pajcsics <105979789+KalmanPA@users.noreply.github.com>
2024-02-15 03:07:53 +0100

    Fixed bullet hit problem

--- UnwrapJam/Assets/Scripts/Object Pooling/BulletHit.cs
+++ UnwrapJam/Assets/Scripts/Object Pooling/BulletHit.cs
@@ -22,6 +22,15 @@ public class BulletHit : MonoBehaviour
         }
         else if(other.gameObject.tag != "ParryBox")
         {
+            if (other.gameObject.CompareTag("Enemy"))
+            {
+                if (other.gameObject.TryGetComponent<IDestructible>(out IDestructible destructible))
+                {
+                    Debug.Log("Killing Enemy");
+                    destructible.Destruct();
+                }
+            }
+
             Debug.Log("Non-Mech");
             Collider[] colliders = Physics.OverlapSphere(this.transform.position,_explosionRadius);
             foreach(Collider collider in colliders)
