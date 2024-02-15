fce6040a9864f1bfd31fe618969805b37811a67f
Kálmán Pajcsics <105979789+KalmanPA@users.noreply.github.com>
2024-02-15 02:27:52 +0100

    More map stuff

--- UnwrapJam/Assets/Scripts/SmallMech/KalmanCamera.cs
+++ UnwrapJam/Assets/Scripts/SmallMech/KalmanCamera.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class KalmanCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 5f; // Speed of movement
+
+    public Transform BigMac;
+
+    float smoothTime = 0.25f;
+
+    Vector3 offset;
+
+    Vector3 velocity = Vector3.zero;
+
+    private void Awake()
     {
-        
+        offset = transform.position - BigMac.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
+
+        Vector3 targetPos = BigMac.position + offset;
+
+
+
+        Vector3 pos = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+
+        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+
         
     }
 }
